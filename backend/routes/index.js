f5c75dadd9ba1787cf5b65b7c7a89310760d7ab1
Robert Kauth <kauthr@gmail.com>
2021-10-07 15:26:21 -0700

    removed hello/world test route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,9 +8,4 @@ router.use("/api", apiRouter);
 
 /*****************************Routes*****************************/
 
-router.get("/hello/world", (req, res) => {
-  res.cookie("XSRF-TOKEN", req.csrfToken());
-  res.send("Hello World!");
-});
-
 module.exports = router;
