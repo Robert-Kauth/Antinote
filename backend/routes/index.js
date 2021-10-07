3564bae5e24472574de39e79d4442e3c74131151
Robert Kauth <kauthr@gmail.com>
2021-10-07 09:34:03 -0700

    successfully tested api route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+// Routers
+const apiRouter = require("./api");
+
+router.use("/api", apiRouter);
+
+// Routes
+
 router.get("/hello/world", (req, res) => {
   res.cookie("XSRF-TOKEN", req.csrfToken());
   res.send("Hello World!");
