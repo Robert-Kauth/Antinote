fcf94b5c9074648cb4f2f25cd9bf09a5ffd81eb1
Robert Kauth <kauthr@gmail.com>
2021-10-08 20:26:03 -0700

    working on modal implementation

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -1,21 +1,15 @@
 import React, { useState } from "react";
 import * as sessionActions from "../../store/session";
-import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 import "./LoginForm.css";
 
-const LoginFormPage = () => {
+const LoginForm = () => {
   const dispatch = useDispatch();
-  const sessionUser = useSelector((state) => state.session.user);
 
   const [credential, setCredential] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState([]);
-
-  if (sessionUser) {
-    return <Redirect to="/" />;
-  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -57,4 +51,4 @@ const LoginFormPage = () => {
   );
 };
 
-export default LoginFormPage;
+export default LoginForm;
