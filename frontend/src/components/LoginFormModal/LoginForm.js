802e16cd7ce9bcbfd4a2faf6e41570efdabbf5bd
Robert Kauth <kauthr@gmail.com>
2021-10-09 11:01:01 -0700

    switch css files to modules

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch } from "react-redux";
 
-import "./LoginForm.css";
+import "./LoginForm.module.css";
 
 const LoginForm = () => {
   const dispatch = useDispatch();
--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import LoginFormModal from "../LoginFormModal";
-import "./Navigation.css";
+import "./Navigation.module.css";
 
 const Navigation = ({ isLoaded }) => {
   const sessionUser = useSelector((state) => state.session.user);
