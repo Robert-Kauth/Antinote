35d24c121dc73ff4e1a5896438ac97ad59b7f518
Robert Kauth <kauthr@gmail.com>
2021-10-11 12:57:29 -0700

    added notebooks route

---
+++ backend/routes/api/notebooks.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const asyncHandler = require("express-async-handler");
+const { check } = require("express-validator");
+const { handleValidatoinErros } = require("../../utils/validation");
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+
+const db = require("../../db/models");
+
+const router = express.Router();
+
+/*****************************Middleware*****************************/
+
+const validateNotebook = [];
+
+/******************************Routes*******************************/
+
+module.exports = router;
