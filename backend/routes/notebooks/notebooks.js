a7ede441aa2ff6c9efd7c1bda5efc7e33ec72977
Robert Kauth <kauthr@gmail.com>
2021-10-12 16:19:00 -0700

    added notebooks reducer, actions, thunks, and removed layer of nesting

--- backend/routes/notebooks/notebooks.js
+++ backend/routes/notebooks/notebooks.js
@@ -3,7 +3,11 @@ const express = require("express");
 const asyncHandler = require("express-async-handler");
 const { check } = require("express-validator");
 const { handleValidatoinErros } = require("../../utils/validation");
-const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const {
+  setTokenCookie,
+  requireAuth,
+  restoreUser,
+} = require("../../utils/auth");
 
 const db = require("../../db/models");
 
@@ -14,5 +18,23 @@ const router = express.Router();
 const validateNotebook = [];
 
 /******************************Routes*******************************/
+// GET /notebooks => return all users notebooks
+router.get(
+  "/",
+  requireAuth,
+  asyncHandler(async (req, res, next) => {
+    const { user } = req.user;
+    const userId = user.id;
+    console.log(user);
+
+    const notebooks = await db.Notebooks.findAll({
+      where: {
+        userId,
+      },
+    });
+    console.log(notebooks);
+    return res.json(notebooks);
+  })
+);
 
 module.exports = router;
---
+++ frontend/src/store/notebooks.js
@@ -0,0 +1,60 @@
+import { csrfFetch } from "./csrf";
+/*-------------Types-------------*/
+const LOAD_NOTEBOOKS = "notebook/load";
+const ADD_NOTEBOOK = "notebook/add";
+const UPDATE_NOTEBOOK = "notebook/update";
+const REMOVE_NOTEBOOK = "notebook/remove";
+/*-------------ACTIONS-------------*/
+
+const load = (notebooks) => {
+  return {
+    type: LOAD_NOTEBOOKS,
+    notebooks,
+  };
+};
+
+const add = (notebook) => {
+  return {
+    type: ADD_NOTEBOOK,
+    notebook,
+  };
+};
+
+const update = (notebookId) => {
+  return {
+    type: UPDATE_NOTEBOOK,
+    notebookId,
+  };
+};
+
+const remove = (notebookId) => {
+  return {
+    type: REMOVE_NOTEBOOK,
+    notebookId,
+  };
+};
+
+/*-------------THUNK CREATORS-------------*/
+export const loadNotebooks = () => async (dispatch) => {
+  const res = await csrfFetch("/notebooks");
+  const notebooks = await res.json();
+  console.log(notebooks, "***********");
+  dispatch(load(notebooks));
+};
+/*-------------REDUCERS-------------*/
+const initialState = { notebooks: null };
+
+const notebooksReducer = (state = initialState, action) => {
+  let newState;
+  switch (action.type) {
+    case LOAD_NOTEBOOKS: {
+      newState = Object.assign({}, state);
+      newState.notebooks = action.notebooks;
+      return newState;
+    }
+    default:
+      return state;
+  }
+};
+
+export default notebooksReducer;
