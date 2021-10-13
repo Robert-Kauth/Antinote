52f2ce29999d4aff456e858a2c31d6232a0226f2
Robert Kauth <kauthr@gmail.com>
2021-10-13 11:17:01 -0700

    added slice of state for notes

---
+++ frontend/src/store/notes.js
@@ -0,0 +1,17 @@
+import { csrfFetch } from "./csrf";
+/*-------------Types-------------*/
+
+/*-------------ACTIONS-------------*/
+
+/*-------------THUNK CREATORS-------------*/
+
+/*-------------REDUCERS-------------*/
+const initialState = {};
+const notesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default notesReducer;
