3069b3e0daf840380452c152a09347b5a24a102f
Robert Kauth <kauthr@gmail.com>
2021-10-14 21:58:18 -0700

    working on notebook cards displaying the right notes

--- frontend/src/components/Notebooks/NotebookCard.js
+++ frontend/src/components/Notebooks/NotebookCard.js
@@ -4,24 +4,21 @@ import { Link } from "react-router-dom";
 import { deleteNote, loadNotes } from "../../store/notes";
 import styles from "./NoteBookCard.module.css";
 
-const NoteBookCard = ({ notebook }) => {
+const NoteBookCard = ({ notebooks }) => {
   const dispatch = useDispatch();
-  const notes = useSelector((state) => Object.values(state.notes));
 
-  const notebookNotes = notes.filter((note) => note.notebookId === note.id);
-
-  useEffect(() => {
-    dispatch(loadNotes());
-  }, [dispatch]);
-
+  const notes = notebooks.map((notebook) => notebook.Notes).flat();
+  console.log(notes);
   //Todo dispatch action to edit current note
   const handleEdit = (e) => {
     e.preventDefault();
   };
-
+  // useEffect(() => {
+  //   dispatch(loadNotes());
+  // }, [dispatch]);
   return (
     <div>
-      {notebookNotes.map((note) => (
+      {notes.map((note) => (
         <span key={note.id} className={styles.wrapper}>
           <div className={styles.noteTitle}>{note.title}</div>
           <div className={styles.noteContent}>{note.content}</div>
