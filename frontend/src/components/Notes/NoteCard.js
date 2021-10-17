b67117267fc57846bde34e96aeb00091923a48c1
Robert Kauth <kauthr@gmail.com>
2021-10-17 10:35:14 -0700

    removed background colors on cards

--- frontend/src/components/Notes/NoteCard.js
+++ frontend/src/components/Notes/NoteCard.js
@@ -3,16 +3,14 @@ import styles from "./NoteCard.module.css";
 
 const NoteCard = ({ notes }) => {
   return (
-    <div className={styles.globalWrapper}>
-      <div className={styles.notesWrapper}>
-        {notes.map((note) => (
-          <span key={note.id} className={styles.wrapper}>
-            <div className={styles.notebookTitle}>{note.Notebook?.title}</div>
-            <div className={styles.noteTitle}>{note.title}</div>
-            <div className={styles.noteContent}>{note.content}</div>
-          </span>
-        ))}
-      </div>
+    <div className={styles.noteCardWrapper}>
+      {notes.map((note) => (
+        <span key={note.id} className={styles.wrapper}>
+          <div className={styles.notebookTitle}>{note.Notebook?.title}</div>
+          <div className={styles.noteTitle}>{note.title}</div>
+          <div className={styles.noteContent}>{note.content}</div>
+        </span>
+      ))}
     </div>
   );
 };
