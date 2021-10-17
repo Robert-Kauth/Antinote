96b16c5826b19a24362ff0154c3fe35bdaf63f98
Robert Kauth <kauthr@gmail.com>
2021-10-17 15:25:58 -0700

    removed unecessary imports from notebookcards

--- frontend/src/components/Notebooks/NotebookCard.js
+++ frontend/src/components/Notebooks/NotebookCard.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import EditCardModal from "./EditCardModal";
-import { deleteNote, loadNotes } from "../../store/notes";
+import { deleteNote } from "../../store/notes";
 
 import styles from "./NoteBookCard.module.css";
 
