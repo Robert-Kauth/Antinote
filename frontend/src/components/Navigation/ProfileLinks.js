8e64b9de482863523ad8bc3f83eb5e13c83e1f1d
Robert Kauth <kauthr@gmail.com>
2021-10-17 19:48:32 -0700

    removed search bar from nav bar =(

--- frontend/src/components/Navigation/ProfileLinks.js
+++ frontend/src/components/Navigation/ProfileLinks.js
@@ -2,21 +2,11 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import CreateModal from "../Notebooks/CreateModal";
 import CreateNoteModal from "../Notebooks/CreateNoteModal";
-import SearchBar from "./Search";
 import styles from "./Navigation.module.css";
-import styled from "styled-components";
 
-const StyledSearch = styled(SearchBar)`
-  position: relative;
-  left: 50vw;
-  right: 50vw;
-`;
 export const ProfileLinks = () => {
   return (
     <div className={styles.profileLinksWrapper}>
-      <span className={styles.searchbar}>
-        <StyledSearch />
-      </span>
       <div>
         <button className={styles.notesButton}>
           <NavLink
