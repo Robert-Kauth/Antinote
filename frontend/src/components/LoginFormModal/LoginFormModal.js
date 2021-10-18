104f43392819e9574226e056f23d0768c7f7eeba
Robert Kauth <kauthr@gmail.com>
2021-10-18 16:57:02 -0700

    added login button modal styling

--- frontend/src/components/LoginFormModal/LoginFormModal.js
+++ frontend/src/components/LoginFormModal/LoginFormModal.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import { Modal } from "../../context/Modal";
 import LoginForm from "./LoginForm";
+import styles from "./LoginForm.module.css";
 
 const LoginFormModal = () => {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <button onClick={() => setShowModal(true)}>Log In</button>
+      <button className={styles.modalButton} onClick={() => setShowModal(true)}>
+        Log In
+      </button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <LoginForm />
