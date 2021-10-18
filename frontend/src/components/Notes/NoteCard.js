14deffd74d2825856ae46538e862557fadeca363
Robert Kauth <kauthr@gmail.com>
2021-10-18 11:24:37 -0700

    working on notes page updating upon deletion of notes/notebooks

--- frontend/src/components/Notes/NoteCard.js
+++ frontend/src/components/Notes/NoteCard.js
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { loadNotebooks } from "../../store/notebooks";
+import { loadNotes } from "../../store/notes";
 import styles from "./NoteCard.module.css";
 
-const NoteCard = ({ notes }) => {
+const NoteCard = () => {
+  const dispatch = useDispatch();
+  const notes = useSelector((state) => Object.values(state.notes));
+  const notebooks = useSelector((state) => Object.values(state.notebooks));
+  console.log(notebooks);
+  useEffect(() => {
+    dispatch(loadNotebooks());
+    dispatch(loadNotes());
+  }, [dispatch]);
+
+  if (!notes.length || !notebooks.length) return null;
+  // This doesn't work at all
   return (
     <div className={styles.noteCardWrapper}>
-      {notes &&
-        notes.map((note) => (
-          <span key={note.id} className={styles.wrapper}>
-            <div className={styles.notebookTitle}>{note.Notebook?.title}</div>
-            <div className={styles.noteTitle}>{note.title}</div>
-            <div className={styles.noteContent}>{note.content}</div>
-          </span>
-        ))}
+      {notebooks.Notes?.map((note, i) => (
+        <span key={i} className={styles.wrapper}>
+          <div className={styles.notebookTitle}>{note.title}</div>
+          <div className={styles.noteTitle}>{note.title}</div>
+          <div className={styles.noteContent}>{note.content}</div>
+        </span>
+      ))}
     </div>
   );
+
+  // This return works but doesn't update properly when notes/ notebooks are deleted
+  // return (
+  //   <div className={styles.noteCardWrapper}>
+  //     {notes &&
+  //       notes.map((note) => (
+  //         <span key={note.id} className={styles.wrapper}>
+  //           <div className={styles.notebookTitle}>{note.Notebook?.title}</div>
+  //           <div className={styles.noteTitle}>{note.title}</div>
+  //           <div className={styles.noteContent}>{note.content}</div>
+  //         </span>
+  //       ))}
+  //   </div>
+  // );
 };
 
 export default NoteCard;
